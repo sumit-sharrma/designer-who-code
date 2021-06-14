650a65a1fdd425dd93a6e306e50b0d428c0eb3e9
Sumit Sharma <sumit13.sharma@paytm.com>
2021-06-15 01:25:00 +0530

    about page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         document.getElementById("header").className = "header header__shadow";
     } else {
         document.getElementById("header").className = "header";
